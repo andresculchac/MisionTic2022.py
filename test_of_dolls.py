df22e471777039925a020af51eda63f8f79b9594
Andres Culchac <mompyrri2001@gmail.com>
2022-04-29 03:09:54 -0500

    Mi primer ejercicio en python

---
+++ test_of_dolls.py
@@ -0,0 +1,7 @@
+#payasos y mu;ecas
+# Escribir un programa que lea el número de payasos y muñecas vendidos en el último pedido
+clown = int(input("How many clow your sell"))
+dolls = int(input("How many dolls your sell"))
+weight = clown * 112 + dolls * 75
+print(clown + dolls, "Y su peso total es de ", weight,"gr")
+#  calcule el peso total del paquete que será enviado.

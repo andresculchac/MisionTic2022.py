3417def373d9b41c5c1c3c59aff72ba2d4551521
Andres Culchac <mompyrri2001@gmail.com>
2022-04-29 23:44:24 -0500

    acabe la primera actividad de ciclo 1 y vamos por mas

--- actividad_ciclo_first.py
+++ actividad_ciclo_first.py
@@ -1,8 +1,20 @@
+# Formula 
 h = int(input())
 l = int(2*h + 4)
 c = int((l + h) / 5)
 
 print(h, l, c)
+# Elaboracion de cafe 
+cafe = c
+if (cafe >=0 and cafe <=20):
+    print("uno")
+if (cafe >=21 and cafe <=30):
+    print("dos")
+if (cafe >=31 and cafe <=50):
+    print("tres")
+if (cafe >=51):
+    print("cuatro")
+
 
 
 
--- test_of_dolls.py
+++ test_of_dolls.py
@@ -1,7 +1,15 @@
 #payasos y mu;ecas
 # Escribir un programa que lea el número de payasos y muñecas vendidos en el último pedido
-clown = int(input("How many clow your sell"))
-dolls = int(input("How many dolls your sell"))
-weight = clown * 112 + dolls * 75
-print(clown + dolls, "Y su peso total es de ", weight,"gr")
-#  calcule el peso total del paquete que será enviado.
+# clown = int(input("How many clow your sell"))
+# dolls = int(input("How many dolls your sell"))
+# weight = clown * 112 + dolls * 75
+# print(clown + dolls, "Y su peso total es de ", weight,"gr")
+# #  calcule el peso total del paquete que será enviado.
+
+# Clasificacion de categorias
+categorias = int(input())
+if (categorias >= 0 and categorias <20):
+    print("categoria 1")
+else:
+    print('categoria demas')
+    

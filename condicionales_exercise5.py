149a338e040a69aad803d126bc9b05af1503f3aa
Andres Culchac <mompyrri2001@gmail.com>
2022-05-08 04:02:53 -0500

    termino el ejercicio de condicionales pues la parte para los hombres  y lo documento muy bien para mi yo del futuro

--- condicionales_exercise5.py
+++ condicionales_exercise5.py
@@ -3,14 +3,24 @@
 # . Escribir un programa que pregunte al usuario su nombre y sexo, y muestre por pantalla el grupo que le corresponde.
 sex_students = input("what is your gender?") 
 name_students = input("What's your name?")
-name = (name_students[0])
+name = (name_students[0]) #con esta variable toma la primera letra de el nombre
 
+# asigno las letras a las variables para despues ver si coinciden name con la letra que piden ps
 grupo_a = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm']
-grupo_aa = ["abcdefghijklm"]
+grupo_a_male = ["n", "ñ", "o", "p", "q", "r", "s", "t", "u",  "v", "w", "x", "y", "z"]
 
+
+# condicionales para mujeres 
 if sex_students == "female":
-    if name in grupo_a:
+    if name in grupo_a:   #el in sirve para preguntar si la variable name esta dentro de la variable grupo a 
         print("pertenece al grupo a ")
     elif name != grupo_a:
             print("perteneces al grupo b")
+
+# condicionales para hombres 
+if sex_students == "male":
+    if name in grupo_a_male:
+        print("pertenece al grupo a ")
+    elif name != grupo_a_male:
+        print("perteneces al grupo b")
 

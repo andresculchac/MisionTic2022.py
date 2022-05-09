0dcfd6c694b1c1e5a85c182dbbc73df40fd0ff59
Andres Culchac <mompyrri2001@gmail.com>
2022-05-09 15:23:48 -0500

    for range and split and replace

---
+++ for_in_range.py
@@ -0,0 +1,10 @@
+# cosas que aprendi primero que todo se tiene que imprimir
+# luego se puede darle una variable sin anotarla directamente
+# toca cerrar la funcion como cualquier otra
+# rango tiene (inicio, tama;o y incremento )
+for date in range(5):
+    print(date)
+
+# contar numeros de 1 a 20
+for number in range (21):
+    print(number)

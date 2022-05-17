eadf5c35c2978907a55756321da6510a4a6f17a8
Andres Culchac <mompyrri2001@gmail.com>
2022-05-16 20:21:09 -0500

    Me quedo, se me daña la funcion por no ser lo suficientemente analitico para pensar mmas a futuro por lo tanto tengo que crear bien la funcion que funcione para los numeros que le ponga y se adapte a la cantidad que hay, tambien tengo pendiente buscar la solucion para hacer que el numero de letras se adapte a la impresion

--- actividad_ciclo_second.py
+++ actividad_ciclo_second.py
@@ -6,7 +6,9 @@ cadena_nestor = input().upper()
 cadena_jurado = input().upper()
 sumatoria_para_pedro = 0     #LO DE AQUI VAN A SER LAS SUMATORIAS 
 sumatoria_para_nestor = 0
-
+numero_de_posicion_en_len = range(len(cadena_jurado))
+ensayo = list (numero_de_posicion_en_len)
+print(ensayo)
 
 # Lo que deberiamos hacer es hacer como una cadena que analice a todos de una vez
 # pero tambien como un contador que diga que si tiene un punto se imprima otro punto pero no se como dibujarlo
@@ -25,26 +27,42 @@ def primeraletra ():
             break        
     else:
         print("L")
-        
+
 primeraletra()
 
-for letras in cadena_jurado:   
-    if cadena_jurado[1] in cadena_pedro[1]:
-        print("punto para pedro vale huevo porque maldita sea  ")
-        sumatoria_para_pedro + 2
-        break 
-    elif cadena_jurado[1] in cadena_nestor[1]:
-        print("punto para nestor ")
-        sumatoria_para_nestor + 2
-        break
-    elif sumatoria_para_pedro == 1:
-        print("J")
-        break
-    elif sumatoria_para_nestor == 1:
-        print("J")
-        break
-else:
-    print("L")
+def len_resto_de_las_letras(posiciones):
+    for letras in cadena_jurado:   
+        if cadena_jurado[posiciones] in cadena_pedro[posiciones]:
+            print("punto para pedro vale huevo porque maldita sea  ")
+            sumatoria_para_pedro + 2
+            break 
+        elif cadena_jurado[posiciones] in cadena_nestor[posiciones]:
+            print("punto para nestor ")
+            sumatoria_para_nestor + 2
+            break
+        elif sumatoria_para_pedro == 1:
+            print("J")
+            break
+        elif sumatoria_para_nestor == 1:
+            print("J")
+            break
+    else:
+        print("L")
+
+len_resto_de_las_letras(1)
+# len_resto_de_las_letras(2)
+# len_resto_de_las_letras(3)
+# len_resto_de_las_letras(4)
+
+
+
+
+
+
+
+
+
+
 
 
     

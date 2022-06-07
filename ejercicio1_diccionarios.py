fdb315fd8e0a783a1fcc85875aea277d085bbd08
Andres Culchac <mompyrri2001@gmail.com>
2022-06-07 17:30:03 -0500

    avance con el reto #4

---
+++ ejercicio1_diccionarios.py
@@ -0,0 +1,14 @@
+# Escribir un programa que guarde en una variable el diccionario {'Euro':'€', 'Dollar':'$', 'Yen':'¥'},
+# pregunte al usuario por una divisa y muestre su símbolo o un mensaje de aviso si la divisa no está en el diccionario.
+
+# mi primer diccionario 😍
+
+
+
+
+nombres = {
+    "Jack": 23,
+    "Jorge": 32
+}
+
+print(nombres.pop())

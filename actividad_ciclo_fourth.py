a94412d3cb3fcdf4ffc8e9cab7b55c0d39799b90
Andres Culchac <mompyrri2001@gmail.com>
2022-06-10 01:09:38 -0500

    analice el problema de una manera mas detallada y me di cuenta de que el problema esta en que cuando imprimimos la llave pues se genera una lista conforme al orden del diccionario y no conforme la lista que yo le propongo por eso vamos a ir por partes

--- actividad_ciclo_fourth.py
+++ actividad_ciclo_fourth.py
@@ -2,17 +2,30 @@
 En este codigo vamos a trabajar con diccionarios 
 Date : 6/8/2022
 
+Code of test
+output        = {"t":66,"u":72,"d":90,"r":84,"j":36,"g":50,"s":94,"q":62,"f":35}
+output_letter = d p h u i e t q
+
 '''
 # importar esta libreria nos permite convertir un string con
 # forma de diccionario a un diccionario de verdad 
 import json
 
-entrada      = json.loads(input())
+entrada = {"t":66,"u":72,"d":90,"r":84,"j":36,"g":50,"s":94,"q":62,"f":35}
 #print(type(entrada)) 
-lista = input() #Lista de entrada
+lista = "d p h u i e t q" #Lista de entrada
 
 contador = 0
 lista2 = []
+
+# The items() method will return each item in a dictionary, as tuples in a list.
+# i think that i must think in the challenge three as through comparation
+# Tengo que solucionar el orden de comparacion
+# idear un plan para que se pongan en orden como la salida de output_letter
+# en debuggin podemos ver como entra el valor con t 66 puesto que es la que comienza 
+# entonces hay radica nuestro problema 
+# ya lo tenemos casi hecho el paso siguente es que lista tenga prioridad y eso como lo hacemos 
+
 
 for llave,valor in entrada.items():
     if llave in lista:

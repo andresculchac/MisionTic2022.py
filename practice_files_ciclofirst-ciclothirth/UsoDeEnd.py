094363b097595e14bb65d7c54e1506175f253181
Andres Culchac <mompyrri2001@gmail.com>
2022-06-20 13:30:47 -0500

    aprendiendo el uso de end=''

---
+++ practice_files_ciclofirst-ciclothirth/UsoDeEnd.py
@@ -0,0 +1,13 @@
+"""
+Es importante saber las formas de imprimir por eso hoy voy aprender los metodos
+a continuación hoy veremos subject: end=""
+"""
+print("Welcome to" , end = ' ') 
+print("GeeksforGeeks", end = ' ')
+print("Andres", end = ' ')
+print("Hola\nYamith")
+a = [1, 2, 3, 4, 5]
+print(a)
+for i in a:
+    print(i, end = '///')
+

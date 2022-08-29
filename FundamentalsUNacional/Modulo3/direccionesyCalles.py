0d9abc0b25bd06a054a69e51ffdeccbf3bdc8605
Andres Culchac <mompyrri2001@gmail.com>
2022-08-29 10:14:33 -0500

    Agrego el titulo para hacer despues los diagramas

--- FundamentalsUNacional/Modulo3/direccionesyCalles.py
+++ FundamentalsUNacional/Modulo3/direccionesyCalles.py
@@ -1,14 +1,14 @@
 #Multiplo de 13 
 #Solo trabajo con multiplo de 13
-
+#Multiplos de 23
 numCalle = int(input())
 numCasa = int(input())
 
-if numCalle%13 == 0:
-    if numCasa%13 == 0:
+if numCalle%13 and numCalle%23 == 0:
+    if numCasa%13 and numCalle%23 == 0:
         print("La direccion calle",numCalle,"#",numCasa,"esta prohibida")
-elif numCalle%13 != 0:
-    if numCasa%13 != 0:
+elif numCalle%13 and numCalle%23 != 0:
+    if numCasa%13 and numCalle%23 != 0:
         print("La direccion calle",numCalle,"#",numCasa,"no esta prohibida")
 
 

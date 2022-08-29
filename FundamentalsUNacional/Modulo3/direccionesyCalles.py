1eead044a64fafac43031867ff55ea9721360ef8
andresc <mompyrri2001@gmail.com>
2022-08-29 11:26:59 -0500

    cambios que ni se para que sirven

--- FundamentalsUNacional/Modulo3/direccionesyCalles.py
+++ FundamentalsUNacional/Modulo3/direccionesyCalles.py
@@ -5,20 +5,11 @@ numCalle = int(input())
 numCasa = int(input())
 
 
-if numCalle%13 and numCalle%23 == 0:
-    if numCasa%13 and numCalle%23 == 0:
+if numCalle%13 == 0:
+    if numCasa%13 == 0:
         print("La direccion calle",numCalle,"#",numCasa,"esta prohibida")
-elif numCalle%13 and numCalle%23 != 0:
-    if numCasa%13 and numCalle%23 != 0:
+elif numCalle%13 != 0:
+    if numCasa%13 != 0:
         print("La direccion calle",numCalle,"#",numCasa,"no esta prohibida")
 
-
-if numCalle%13  == 0:
-    print("La direccion calle",numCalle,"#",numCasa,"esta prohibida")
-elif numCasa%13 == 0:
-    print("La direccion calle",numCalle,"#",numCasa,"esta prohibida")
-
-
-elif numCalle%13 != 0: #osea no es multiplo
-    if numCasa%13 != 0:
-        print("La direccion calle",numCalle,"#",numCasa,"no esta prohibida")   
+   

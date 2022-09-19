763fa15bf77c8d6a82518e30527f331705ad48f4
andresc <mompyrri2001@gmail.com>
2022-09-19 08:24:51 -0500

    actualizo impresion mensaje y agrego sobre los ejes x,y los negativos

--- FundamentalsUNacional/Modulo 4/cuadrantes.py
+++ FundamentalsUNacional/Modulo 4/cuadrantes.py
@@ -3,15 +3,15 @@ yy = float(input())
 
 #algorithm to axes
 #no sabia que se podia agrupar en un parentesis dos cosas como estas y salen los parentesis
-if xx > 0.0 and yy == 0.0:
-    print("La coordenada",(xx, yy),"esta sobre el eje X")
-elif xx == 0.0 and  yy > 0.0 :
+if (xx > 0.0 or xx< -0.0) and yy == 0.0 :
+    print("La coordenada",(xx, yy),"esta sobre el eje X") 
+elif xx == 0.0 and  (yy > 0.0 or yy < -0.0):
         print("La coordenada",(xx, yy),"esta sobre el eje Y")  
 elif xx == 0.0  and yy == 0.0:
         print("La coordenada",(xx, yy),"corresponde al origen")
 #for quadrants
 if xx > 0.0 and yy > 0.0:
-    print("La coordenada(",xx,yy,")se encuentra en el cuadrante 1")
+    print("La coordenada",(xx, yy),"se encuentra en el cuadrante 1")
     
 elif xx < -0.0 and yy > 0.0:
     print("La coordenada",(xx, yy),"se encuentra en el cuadrante 2")

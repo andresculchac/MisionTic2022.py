31b870818eb0c221d64824bc0cc5fe6d13626c65
andresc <mompyrri2001@gmail.com>
2022-09-19 11:12:45 -0500

    identifico al major y al mejor

---
+++ FundamentalsUNacional/Modulo 4/mediana.py
@@ -0,0 +1,10 @@
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+
+if num1 > num2 and num1 > num3:
+    print("num1 this is the major number ")
+if num2 > num1 and num2 > num3:
+    print("num2 this is the major number ")
+if num3 > num1 and num3 > num2:
+    print("num3 this is the major number ")
--- FundamentalsUNacional/Modulo3/calificacionesenUsa.py
+++ FundamentalsUNacional/Modulo3/calificacionesenUsa.py
@@ -4,7 +4,7 @@ if letterUsa >= 90:
 elif letterUsa >= 80 and letterUsa < 90:
     print("El porcentaje",letterUsa,"corresponde a la calificacion B")
 elif letterUsa >= 70 and letterUsa < 80:
-    print("El porcentaje",letterUsa,"corresponde a la calificacion E")
+    print("El porcentaje",letterUsa,"corresponde a la calificacion C")
     #print("El porcentaje 70.0 corresponde a la calificacion E")
 elif letterUsa >= 60 and letterUsa < 70:
     print("El porcentaje",letterUsa,"corresponde a la calificacion D")
@@ -12,4 +12,4 @@ elif letterUsa >= 40 and letterUsa < 60:
     print("El porcentaje",letterUsa,"corresponde a la calificacion E")
 else:
     print("El porcentaje",letterUsa,"corresponde a la calificacion F")
-    print("El porcentaje 70.0 corresponde a la calificacion E")
+    #print("El porcentaje 70.0 corresponde a la calificacion E")

825007818916934671f648f1b5e08ddbf4021a08
andresc <mompyrri2001@gmail.com>
2022-09-20 08:52:18 -0500

    se elimina los identificadores de mayor o menor y se realiza literal el ejercicio

--- FundamentalsUNacional/Modulo 4/mediana.py
+++ FundamentalsUNacional/Modulo 4/mediana.py
@@ -2,9 +2,18 @@ num1 = float(input())
 num2 = float(input())
 num3 = float(input())
 
-if num1 > num2 and num1 > num3:
-    print("num1 this is the major number ")
-if num2 > num1 and num2 > num3:
-    print("num2 this is the major number ")
-if num3 > num1 and num3 > num2:
-    print("num3 this is the major number ")
+if num1 < num2 and num3 > num1 and num2 > num3:
+    print((num1 + num2 + num3)/3)
+#3.0 4.0 5.0 ejemplo para que de la mediana
+elif num1 <num2 and num2 < num3 and num3 > num1:
+    print(num2,"es la mediana")
+# 5.0 3.0 1.0 ejemplo para esta 
+elif num1 > num2 and num2 > num3 and num3 < num1:
+    print(num1, "es la mediana")
+# 444.44 555.55 333.33
+elif num1 < num2 and num2 > num3 and num3 < num1:
+    print(num1,"es la mediana")
+if num1 > num2 and num2 < num3 and num3 == num1:
+    print(num1,"es la mediana")
+if num1 == num2 and num2 == num3  and num3 == num1:
+    print(num1,"es la mediana")

159b957bcb9f30ed45f2a5e746a4b8694f2b6a62
andresc <mompyrri2001@gmail.com>
2022-09-25 11:51:36 -0500

    podran cuestionar mis metodos pero no mis resultados

--- FundamentalsUNacional/Modulo 5/banderaInglaterra.py
+++ FundamentalsUNacional/Modulo 5/banderaInglaterra.py
@@ -1,17 +1,14 @@
 ancho = int(input())
 altura = int(input())
-mitad = altura /2 +0.5
+mitad = altura/2+0.5
 mitadAncho = ancho /2 +0.5
 if (ancho%2 != 0 and altura%2 != 0) < 20:
     #son primos y menores a 20
-    
-    for i in range(1,altura+1):  #mejor hacer un algorithm que cuando llegue la mediana se ponga una "0" despues que lo logremos solo es duplicar
-
+    for j in range(1,altura+1):
+        j = "+"
+    for i in range(1,altura+1):
         if i == mitad:
-            i = "0"
+            i = "+"
         else:
-            i = "X"
-        print(i)
-    print("\n")
-    
-        
+            i = "0"
+        print(str(i)*int(mitadAncho-1)+str(j)+str(i)*int(mitadAncho-1))

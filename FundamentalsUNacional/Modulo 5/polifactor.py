087e7e43bd33456734963d726c07d777fe7acb5b
andresc <mompyrri2001@gmail.com>
2022-09-27 09:22:25 -0500

    finalmente pudimos lograr la impresion. Encontramos la suma total que hay en los for(num1, num2 ) si resto en num2 y el num1 sale el numero que tanto me costo

--- FundamentalsUNacional/Modulo 5/polifactor.py
+++ FundamentalsUNacional/Modulo 5/polifactor.py
@@ -2,14 +2,22 @@ num1 = int(input())
 num1Estatico = num1
 num2 = int(input())
 num3 = int(input())
+acumulador = 0 
 
 for i in range(num2, num3+1):
-    factor =num1/i
-    num1 = factor
-    # if factor%i ==0:
-    #     print(factor)
-if num1 == 1:
-    print(num1Estatico,"es polifactor entre",num2,"y",num3)    
-else:
-    print(num1Estatico,"no es polifactor entre",num2,"y",num3)
+    if num1 % i == 0:
+        factor = num1 /i
+        num1 = factor
+        acumulador += 1
+    else:
+        print(num1Estatico,"no es polifactor entre",num2,"y",num3)
+
+if acumulador == (num3+1)-num2:
+    print(num1Estatico,"es polifactor entre",num2,"y",num3)
+# print(i, acumulador)
+# print("operacion:" ,(num3+1)-num2)
+#print(num1Estatico,"es polifactor entre",num2,"y",num3) 
+
+# else:
+#     print(num1Estatico,"no es polifactor entre",num2,"y",num3)
     

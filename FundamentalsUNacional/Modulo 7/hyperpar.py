a4fc789dd15e8461d7c6a9825ae18c4784a99a97
andresc <mompyrri2001@gmail.com>
2022-11-17 12:07:40 -0500

    documento el punto 6 modulo 7 y lo finalizo

--- FundamentalsUNacional/Modulo 7/hyperpar.py
+++ FundamentalsUNacional/Modulo 7/hyperpar.py
@@ -1,5 +1,7 @@
 #Modificacion para la formula 
-listaX = []
+#lo siguiente que tengo que hacer es pasar el codigo a una funcion donde reciba
+#la lista de la funcion Listend()
+
 
 def listend(): #Esta funcion recibe inputs y termina cuando le pones un -1
     variableX = True
@@ -9,31 +11,39 @@ def listend(): #Esta funcion recibe inputs y termina cuando le pones un -1
             variableX = False
         listaX.append(variableJ)
     listaX.pop(-1) #Sirve para eliminar el 0 que a veces puede causar interferencias
-    print(listaX) # este print es de referencia para ver como se esta llendo la lista :3
+    #print(listaX) # este print es de referencia para ver como se esta llendo la lista :3
 
-#listend()
+
 
 '''
 Encontre a la solucion hypepares pasandolos a strings de modo que toca ver como los adaptamos
+De este punto aprendi bastante acerca de ciclos y su funcionamiento mas de cerca y como utilizar for in range
+entendiendo un poco mas de for in
+#aprendi acerca de no cometer errores en las comparaciones porque no es lo mismo == que = y es algo que me confundi y estaba comparando
+y por eso no me cambiana la variable de booleanos(gracias a dios me ayudo la tutora muy observadora 👀)
 '''
-x = ['2080','256','88888']
-j = [2080 , 256,88888]
+listaX = [] #Es raro porque si esta variable no exixte antes de la funcion dice que no esta declarada XD
+listend()
 indice = 0
 
-
-for j in range(len(j)):
-    #print(x[indice])
-    for i in x[indice]:
-        print(i)
+encontrador = True
+for i in range(len(listaX)):
+    for i in listaX[i]:
+        if int(i) % 2 == 0:
+            encontrador = True
+        elif int(i)%2 != 0:
+            encontrador = False
+            break
+            
+    if encontrador == False:
+        print("No es hyperpar")
+        encontrador = True
+    elif encontrador == True:
+        print("Hyperpar")
     indice += 1
-    #print(type(i))
+    
     
 
     
       
 
-# print(caracter)
-#     if int(caracter)%2 == 0:
-#       print("Hyperpar")  
-#     else:
-#       print("no es par")

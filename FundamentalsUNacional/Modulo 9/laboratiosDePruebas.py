cd411b586f242b06be624d9cc4317c748af76ae6
andresc <mompyrri2001@gmail.com>
2022-11-20 18:20:17 -0500

    voy a adaptar el lab de pruebas a mi codigo principal por eso podria cagarla asi que registro mis avances

--- FundamentalsUNacional/Modulo 9/laboratiosDePruebas.py
+++ FundamentalsUNacional/Modulo 9/laboratiosDePruebas.py
@@ -1,12 +1,32 @@
 contador = 0
 almacen = []
 listaPorSiAcaso = []
-a = ["obelix","julianicus","pedro","nicolax"]
+
+a = ["obelix","julianicus","pedro","Dhalekanya","nicolax"]
 for i in range(len(a)):
-    contador = 0
+    if a[i][-1:] == "a": #algoritmo para identificar a los indios
+        amabilidad = a[i][-1:]
+        almacen.append(amabilidad)
+        if amabilidad == "a":
+            print("Indios")
+    
     robar =a[i][-2:]
     almacen.append(robar)
-
+    if robar == "ix":
+        print("Galo")
+    if robar == "us":
+        print("Romano")
+    if robar == "ic":
+        print("Godos")
+    if robar == "as":
+        print("Griegos")
+    if robar == "af":
+        print("Normandos")    
+    if robar == "is" or robar == "ax":
+        print("Bretones")
+    if robar == "ez":
+        print("Hispanos")
+    
             
             
             
--- FundamentalsUNacional/Modulo 9/personaje.py
+++ FundamentalsUNacional/Modulo 9/personaje.py
@@ -1,17 +1,15 @@
 
+#Todos se tienen que convertir con la primera en mayuscula.
 
-def inputs(): #hacer una funcion que si pongo un numero input pues se ejecute tres veces #tipos de inputs
+def inputs(): #hacer una funcion que si pongo un n inputs pues se ejecute n veces #tipos de inputs
     request = int(input())
     while request>0:
-      j = input() #Se evalua el tipo de entrada que se le va a poner ej: int, float, str
+      j = input().capitalize() #Se evalua el tipo de entrada que se le va a poner ej: int, float, str ademas se pone el manejo de strings
       listaX.append(j)
       request -= 1
     print(listaX)
 
-listaX = ['obelix'] #sufijo 'ix' significa origen "galo"   
+listaX = [] #sufijo 'ix' significa origen "galo"   
+inputs()
 
 
-
-# for i in range(len(listaX)): #Ciclo que analiza en una lista letra por letra.
-#     for i in listaX[i]:
-#         if i == listaX[0]

6577ab2c949e3ecf10c847b5a6d033c9fbae3a5d
andresc <mompyrri2001@gmail.com>
2023-02-20 16:22:19 -0500

    Guardo pero ya ni entiendo

--- FundamentalsUNacional/Modulo 6/parejasGanadoras.py
+++ FundamentalsUNacional/Modulo 6/parejasGanadoras.py
@@ -1,12 +1,28 @@
-variableX = True
-listaX = []
-while variableX:
-    variableJ = int(input())
-    if variableJ == 0:
-        variableX = False
-    listaX.append(variableJ) #inputs hasta poner el 0 se para 
+# variableX = True
+# listaX = []
+# while variableX:
+#     variableJ = int(input())
+#     if variableJ == 0:
+#         variableX = False
+#     listaX.append(variableJ) #inputs hasta poner el 0 se para 
 
-#despues hacer un for para que sume uno con uno xd
+# #despues hacer un for para que sume uno con uno xd
 
-for i in listaX:
-    print(i)
+# for i in listaX:
+#     print(i)
+'''Eso era mio xd'''
+
+
+ganadores=[ ]
+keyx=0
+while True:
+  vine=int(input())
+  if vine==0:
+    break
+  else:
+    ganadores.append(vine)
+for j in ganadores:
+  complemento=1995-j
+  if complemento in ganadores:
+    keyx+=1
+print(keyx//2)

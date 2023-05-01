4298d5c4c01f336f9fa9663892e8f089d9181fe8
Andres Yamith <mompyrri2001@gmail.com>
2023-05-01 10:52:26 -0500

    Wait a minute i am think in xournal

--- FundamentalsUNacional/Modulo 5/banderaparte2.py
+++ FundamentalsUNacional/Modulo 5/banderaparte2.py
@@ -1,7 +1,11 @@
-l,h = int(input()),int(input())
+width,height = int(input()),int(input())
 #The Prints going to one by one
 #Discover the half
+    #add conditional
 #First Step : Discover the table with zero
 
-for i in range(h):
-    print("0"*l)
+for i in range(height):
+    if height > 0:
+        height // 2
+        print("0")
+    

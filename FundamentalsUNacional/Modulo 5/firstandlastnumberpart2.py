3ababcc438c4203bedabb3cf43372f4e53a1c562
Andres Yamith <mompyrri2001@gmail.com>
2023-05-01 01:00:26 -0500

    First and last number part 2

--- FundamentalsUNacional/Modulo 5/firstandlastnumberpart2.py
+++ FundamentalsUNacional/Modulo 5/firstandlastnumberpart2.py
@@ -1,6 +1,17 @@
 #Pedir el primero y el ultimo numero de una serie de numeros que me pidan
 
-numeros = int(input())
+numeros = input()
+lectura = len(numeros)
+contador = 0
+#hacer un ciclo que me entre uno por uno cada numero
+#
+lista = []
+for numero in numeros:
+    contador += 1
+    if contador == 1:
+        agregar = lista.append(numero)
+    if contador == lectura:
+        agregar = lista.append(numero)
 
-
+print(int(lista[0]+lista[1]))
 

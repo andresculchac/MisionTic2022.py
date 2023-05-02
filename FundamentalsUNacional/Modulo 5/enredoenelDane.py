49ff655d318efacdfdc10920945031cb34fe7dc0
Andres Yamith <mompyrri2001@gmail.com>
2023-05-02 07:25:50 -0500

    I finished the point and i understand the point

--- FundamentalsUNacional/Modulo 5/enredoenelDane.py
+++ FundamentalsUNacional/Modulo 5/enredoenelDane.py
@@ -1,10 +1,14 @@
-pedirnum = int(input())
-
 pedirEntrada = int(input())
 ListEmpty = []
+remainder = 0 #stack of numbers day
 
 for i in range(1,pedirEntrada+1):
-    requestInput = int(input()) #The types of variable  can change
-    addList = ListEmpty.append(requestInput)
-print(ListEmpty)
+    requestInput = float(input())
+    myself = requestInput*i-remainder
+    remainder += myself
+    transformation = int(myself)
+    addList = ListEmpty.append(transformation)
+
+for i in ListEmpty:
+    print(i)
 

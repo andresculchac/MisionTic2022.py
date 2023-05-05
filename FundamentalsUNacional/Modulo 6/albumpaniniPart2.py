00871ac78ca42d569ad217acd1c4013a234f8643
Andres Yamith <mompyrri2001@gmail.com>
2023-05-05 09:16:32 -0500

    I delivery the input the point

--- FundamentalsUNacional/Modulo 6/albumpaniniPart2.py
+++ FundamentalsUNacional/Modulo 6/albumpaniniPart2.py
@@ -1,7 +1,11 @@
 x = True  #The point require find in a list the only numbers and if they repeat delete it.
+lista = []
 while x:
-    inputs = int(input())
-    if inputs == 0:
+    y = int(input())
+    request = lista.append(y)
+    if y == 0:
         x = False
+lista.pop(-1)
+print(lista)
         
 

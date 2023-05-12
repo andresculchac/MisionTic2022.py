1ba35c5224547593a7b8c908964e07f9ba45b8ed
Andres Yamith <mompyrri2001@gmail.com>
2023-05-12 00:33:41 -0500

    Parece que acabamos el ejercicio

--- FundamentalsUNacional/Modulo 6/batallasCampales.py
+++ FundamentalsUNacional/Modulo 6/batallasCampales.py
@@ -29,11 +29,21 @@ for i in range(int(numLista/2)):
     verdad = lista2.append(y)
     viva =lista.remove(x)
     lau = lista.remove(y)
+
+
+print(lista2)
+for i in range(int(numLista/2)):
+    primerpar = lista2[0]
+    segundopar = lista2[1]
+    if primerpar % 2 == 0 and segundopar % 2 == 0:
+        v = lista2.remove(primerpar)
+        vv = lista2.remove(segundopar)
+        
+    elif primerpar % 2 != 0 and segundopar % 2 != 0:
+        v = lista2.remove(primerpar)
+        vv = lista2.remove(segundopar)
     
 
 
 
-
-
-
-print(lista2)
+print("Sobreviven",len(lista2),"soldados")

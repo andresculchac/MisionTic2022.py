a6a321b2d27e35d60ac3df58bb3985ff2ed17b54
Andres Yamith <mompyrri2001@gmail.com>
2023-05-12 03:29:53 -0500

    I be found the repeat number

---
+++ FundamentalsUNacional/Modulo 6/bingoPart3Yo.py
@@ -0,0 +1,19 @@
+#lista = [3,5,2,1,4,2,2,5,1,5]
+contador = 0
+#algoritmo que identifique cual se repite mas
+
+lista = [1,2,3,4,1]
+lista2 = []
+N = len(lista)
+
+contador = 0
+for i in range(1,N):
+    repitencia = lista[0]
+    if  repitencia == lista[i] :
+        contador += 1
+        lista2.append(lista[i])
+lista.remove(lista[0])
+
+print(lista2)
+
+

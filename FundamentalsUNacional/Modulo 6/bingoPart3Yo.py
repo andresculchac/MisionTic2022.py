23dac206c4f31a1b7698cf3eb3892a807fda320a
Andres Yamith <mompyrri2001@gmail.com>
2023-05-12 03:55:18 -0500

    WIiiiiiiiiiii i found the number repeat in a list

--- FundamentalsUNacional/Modulo 6/bingoPart3Yo.py
+++ FundamentalsUNacional/Modulo 6/bingoPart3Yo.py
@@ -1,18 +1,28 @@
-#lista = [3,5,2,1,4,2,2,5,1,5]
+lista = [3,5,2,1,4,2,2,5,1,5]
 contador = 0
 #algoritmo que identifique cual se repite mas
 
-lista = [1,2,3,4,1]
+#lista = [1,2,3,4,1]
 lista2 = []
 N = len(lista)
 
 contador = 0
-for i in range(1,N):
+# for i in range(1,N):
+#     repitencia = lista[0]
+#     if  repitencia == lista[i] :
+#         contador += 1
+#         lista2.append(lista[i])
+# lista.remove(lista[0])
+
+for i in range(N):
+    N = len(lista)
     repitencia = lista[0]
-    if  repitencia == lista[i] :
-        contador += 1
-        lista2.append(lista[i])
-lista.remove(lista[0])
+    for j in range(1,N):
+        if repitencia == lista[j]:
+            contador += 1
+            lista2.append(lista[j])
+    lista.remove(lista[0])
+
 
 print(lista2)
 

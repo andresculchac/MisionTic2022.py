92696fcdb80166d845c53e0a7c38a1e8fdf69096
Andres Yamith <mompyrri2001@gmail.com>
2023-05-12 14:01:02 -0500

    The point is ready

---
+++ FundamentalsUNacional/Modulo 6/racimodeuvas.py
@@ -0,0 +1,19 @@
+#2023-1
+#Este parece muy facil
+#Primero inputs con 0
+
+# variableX = True
+# listaX = []
+
+# while variableX:
+#     variableJ = int(input())
+#     if variableJ == 0:
+#         variableX = False
+#     listaX.append(variableJ)
+# listaX.pop(-1)
+
+#Falta agregar la formula triangular 
+
+
+
+

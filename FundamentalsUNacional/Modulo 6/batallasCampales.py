eebfea42c408adaeb45574d904c6eb0adb367884
Andres Yamith <mompyrri2001@gmail.com>
2023-05-11 21:17:15 -0500

    First Step: Input Problem

---
+++ FundamentalsUNacional/Modulo 6/batallasCampales.py
@@ -0,0 +1,13 @@
+#lista = [180,175,186,179,181,173,181,170]
+
+#ordenar por mayor y final por ciclos
+
+#First Step
+
+pedirEntrada = int(input())
+ListEmpty = []
+
+for i in range(1,(pedirEntrada*2)+1):
+    requestInput = int(input()) #The types of variable  can change
+    addList = ListEmpty.append(requestInput)
+print(ListEmpty)

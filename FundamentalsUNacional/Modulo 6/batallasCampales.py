7772a48673d5625a5650d726c97aef8641308015
Andres Yamith <mompyrri2001@gmail.com>
2023-05-11 21:47:12 -0500

    Second step

--- FundamentalsUNacional/Modulo 6/batallasCampales.py
+++ FundamentalsUNacional/Modulo 6/batallasCampales.py
@@ -4,10 +4,20 @@
 
 #First Step
 
-pedirEntrada = int(input())
-ListEmpty = []
+# pedirEntrada = int(input())
+# ListEmpty = []
 
-for i in range(1,(pedirEntrada*2)+1):
-    requestInput = int(input()) #The types of variable  can change
-    addList = ListEmpty.append(requestInput)
-print(ListEmpty)
+# for i in range(1,(pedirEntrada*2)+1):
+#     requestInput = int(input()) #The types of variable  can change
+#     addList = ListEmpty.append(requestInput)
+# print(ListEmpty)
+
+#second Step 
+    #Ciclo que organiza los numeros pares e impares bien lindo
+
+lista = [1,6,4,2,3,5]
+lista2 = []
+#lista esperada # [6,1,5,2,4,3]
+numLista = len(lista)
+for i in range (numLista):
+    print(lista[i])

1aba59e0ed631ad38ec6b2382821cc74c50b05af
Andres Yamith <mompyrri2001@gmail.com>
2023-05-11 23:56:15 -0500

    logro organizar las listas a mi gusto

--- FundamentalsUNacional/Modulo 6/batallasCampales.py
+++ FundamentalsUNacional/Modulo 6/batallasCampales.py
@@ -1,4 +1,4 @@
-#lista = [180,175,186,179,181,173,181,170]
+lista = [180,175,186,179,181,173,181,170]
 
 #ordenar por mayor y final por ciclos
 
@@ -15,18 +15,25 @@
 #second Step 
     #Ciclo que organiza los numeros pares e impares bien lindo
 
-lista = [1,6,4,2,3,5]
+
 lista2 = []
 #lista esperada # [6,1,5,2,4,3]
 mayor = lista[0]
 menor = lista[0]
 numLista = len(lista)
-for i in range (1,numLista):
-    if lista[i] > mayor:
-        mayor = lista[i]
 
-    if lista[i] < menor:
-        menor = lista[i]
-x = lista2.append(menor)
-y = lista2.append(mayor)
+for i in range(int(numLista/2)):
+    x = max(lista)
+    y = min(lista)
+    osea = lista2.append(x)
+    verdad = lista2.append(y)
+    viva =lista.remove(x)
+    lau = lista.remove(y)
+    
+
+
+
+
+
+
 print(lista2)

71d6022e10fc65c390dcbf4ea5b594aae5f74adb
Andres Yamith <mompyrri2001@gmail.com>
2023-05-12 22:22:50 -0500

    I finished but i could not complete the poin y require more thinking

--- FundamentalsUNacional/Modulo 6/bingoPart3Yo.py
+++ FundamentalsUNacional/Modulo 6/bingoPart3Yo.py
@@ -19,7 +19,37 @@ for i in range(N):#algoritmo que identifique cual se repite mas, en este caso lo
             lista2.append(lista[j])
     lista.remove(lista[0])
 
+contarlista = len(lista2)
+listaesperada = []
+secondnumcycle = 0
+contador = 0
+contadorEstable = 0
+primernumero = 0
+segundonumero = 0
 
-print(lista2)
+#el numero mas repetido se tiene que poner de primero 
+# vamos por lo sencillo, cuantes veces se repite el 5 en la lista2
+#vamos por la lista2 esperada
+
+for i in range(contarlista):
+    contarlista = len(lista2)
+    firstnumcycle = lista2[0]
+    for j in range(1,contarlista):
+        if firstnumcycle == lista2[j]:
+            contador += 1
+            if lista2[j] not in listaesperada:
+                listaesperada.append(lista2[j])
+    if contador > contadorEstable:
+        contadorEstable = contador
+    elif contador < contadorEstable:
+        segundonumero = firstnumcycle
+    if contador > 0:
+        if contador >= contadorEstable:
+            primernumero = firstnumcycle
+            
+        contador = 0
+    lista2.remove(lista2[0])
+print(primernumero)
+print(segundonumero)
 
 

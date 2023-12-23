dbe7d4fb77e7ffe8224f30522ff2371977f95efb
Andres Yamith <mompyrri2001@gmail.com>
2023-12-23 12:22:02 -0500

    Nueva logica mas simple con returns

--- FundamentalsUNacional/Modulo 8/verificarOrden.py
+++ FundamentalsUNacional/Modulo 8/verificarOrden.py
@@ -1,14 +1,10 @@
-yy = [5,6,7,20,9]
+yy = [5,6,7,8,9]
 
 def orden(lista):
     for i in range(len(lista)):
         if lista[0] + i == lista[i]:
-            if lista[0]+1 == lista[1]:
-                continue
-            else:
-                return False
-                break
-            
+            if lista[0]+1 != lista[1]:
+                return False          
         else:
             return False
             break

3a09e4a60b60c49ffdcbbe2a4a6b9fc05c551dcd
Andres Culchac <81845045+andresculchac@users.noreply.github.com>
2024-04-21 16:49:02 -0500

    actualizado bonita impresion como lo pide la pagina

--- FundamentalsUNacional/Modulo 9/cacharrito.py
+++ FundamentalsUNacional/Modulo 9/cacharrito.py
@@ -1,26 +1,30 @@
 
-
+impresionFinal = []
 #example = "ABC123 DEF456 GHI789"
+NumeroDeSolicitudes = int(input())
+for i in range(NumeroDeSolicitudes):
+    placa = input()
+    jorge = placa.split(" ")
+    MiPlaca = 0
 
-placa = "IAN373 BBB333 ZZZ999"
-jorge = placa.split(" ")
-MiPlaca = 0
-
-
-for i in jorge[0]:
-    MiPlaca += ord(i)
-
-
+    for i in jorge[0]:
+        MiPlaca += ord(i)
 
 
-for i in jorge:
-    contador = 0
-    for j in i:
-        contador += ord(j)
-    if MiPlaca > contador: 
-        print("Al menos otro auto es mas viejo que mi cacharrito :(")   
-        break
+
+
+    for i in jorge:
+        contador = 0
+        for j in i:
+            contador += ord(j)
+        if MiPlaca > contador: 
+            impresionFinal.append(f"Al menos otro auto es mas viejo que mi cacharrito :(")   
+            break
+            
+    if MiPlaca < contador: #Si de mi placa inicial se mantiene igual imprime
+        impresionFinal.append(f"Mi cacharrito es el mas viejo de todos los autos ;D")
         
-if MiPlaca < contador: #Si de mi placa inicial se mantiene igual imprime
-    print("Mi cacharrito es el mas viejo de todos los autos ;D")
+
+for x in impresionFinal:
+    print(x)
         

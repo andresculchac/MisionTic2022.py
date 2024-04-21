b6b1f5530fd82008244d892e3be88909646e7408
Andres Culchac <81845045+andresculchac@users.noreply.github.com>
2024-04-21 16:43:13 -0500

    esta el codigo pero falta la decoración de impresion bonita como pide la hoja

--- FundamentalsUNacional/Modulo 9/cacharrito.py
+++ FundamentalsUNacional/Modulo 9/cacharrito.py
@@ -2,13 +2,25 @@
 
 #example = "ABC123 DEF456 GHI789"
 
-placa = "ZZZ999"
+placa = "IAN373 BBB333 ZZZ999"
+jorge = placa.split(" ")
+MiPlaca = 0
 
 
-contador = 0
-for i in placa:
+for i in jorge[0]:
+    MiPlaca += ord(i)
+
+
+
+
+for i in jorge:
+    contador = 0
     for j in i:
         contador += ord(j)
+    if MiPlaca > contador: 
+        print("Al menos otro auto es mas viejo que mi cacharrito :(")   
+        break
         
-
-print(contador)
+if MiPlaca < contador: #Si de mi placa inicial se mantiene igual imprime
+    print("Mi cacharrito es el mas viejo de todos los autos ;D")
+        

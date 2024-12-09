ce6f04fd6362e48af529d51332df36f2cb503d18
Andres Yamith <mompyrri2001@gmail.com>
2024-12-09 10:36:20 -0500

    Nuevos cambios

--- FundamentalsUNacional/Modulo 11/calendarioColombiano.py
+++ FundamentalsUNacional/Modulo 11/calendarioColombiano.py
@@ -1,4 +1,5 @@
 import datetime 
+import datetime as timedelta
 
 # matrix =[["lun\t","mar\t","mie\t","jue\t","vie\t","sab\t","dom"],
 #          ["\t",2,3,4,5,6]]
@@ -15,16 +16,23 @@ import datetime
 from datetime import datetime
 
 # Crear un objeto de fecha para el 1 de enero de 2010
-fecha = datetime(2010, 1, 1)
+# fecha = datetime(2000,1,0)
+# now = datetime.weekday(fecha)
+#primero el algoritmo, voy a tratarlo como por matrices, creo que diccionarios por el momento no son los adecuados
+#despues agrego las cosas bonitas del ejercicio, pero primero el algoritmo
+#puede ser por diccionarios por ejemplo los diccionarios se van llenando por dias por ejemplo como lunes representa un cero, y todo lo que este asociadoa a un cero representa un numero, por ejemplo cuando llegue lunes y por ejemplo debe dar error, cuando de error , lo solucionamos con try, pero en vez de try se agrege una matriz que me permita agregar tabulaciones hasta cierto punto
+#print(now)
+# calendario = [[0,1,2,3,4,5,6],
+#               [1,2,3,4,5,6,7],
+#               [8,9,10,11,12,13,14],
+#               [15,16,17,18,19,20,21]]
 
-# Obtener el día de la semana (0 para lunes, 1 para martes, ..., 6 para domingo)
-dia_semana = fecha.weekday()
+'''
+¿Cómo saber cual es el dia 
+que comienza el mes cuando pongo un mes aleatorio?
 
-# Lista de nombres de los días de la semana
-nombres_dias_semana = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo']
+'''
 
-# Imprimir el día de la semana en que cae el 1 de enero de 2010
-print("El 1 de enero de 2010 cae en", nombres_dias_semana[dia_semana])
 
 
 
